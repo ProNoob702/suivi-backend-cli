8fa4f7c9a9fe453ee943e9584f1e3af2f7812c47
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2021-11-16 17:03:16 +0100

    newer version

--- bin/index.js
+++ bin/index.js
@@ -117,7 +117,7 @@ using Statsh.EventFlow.Commands;
 
 namespace Statsh.Domain.TT.Commands
 {
-    [RequiredBoardMemberRole(${aggregateName}MemberRole.Admin)]
+    [Required${aggregateName}MemberRole(${aggregateName}MemberRole.Admin)]
     public class ${filename} : ClientSideCommand<${aggregateName}Aggregate, ${aggregateName}Ref>
     {
         public ${filename}(${aggregateName}Ref id, ClientSideCommandRef? commandId): base(id, commandId)
